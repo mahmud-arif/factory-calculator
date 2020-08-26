70892078c1e1ae362a5e561349380fad394038ae
mahmud <mahmudarif1175@gmail.com>
2020-08-26 11:38:03 +0600

    feat: calculate factorial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import { factorial as Factorial } from "./util"
+
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [inputValue, setInputValue] = useState()
+	const [factorial, setFactorial] = useState(0)
+	const [error, setError] = useState()
+
+	const onSubmit = (e) => {
+		e.preventDefault()
+
+		setFactorial(Factorial(inputValue))
+	}
+
+	const inputChangeHandler = (e) => {
+		setError("")
+		const value = parseInt(e.target.value)
+		if (value < 0) {
+			setError("Only enter positive number")
+		}
+		setInputValue(value)
+	}
+
+	return (
+		<div className='container'>
+			<div className='content'>
+				<div className='form-group'>
+					<h1 className='form-group__header'>Factorial Calculator</h1>
+					<form onSubmit={onSubmit} className='form-group__form'>
+						<input
+							type='number'
+							required
+							value={inputValue}
+							placeholder='Enter a number...'
+							onChange={inputChangeHandler}
+							className='form-group__input'
+						/>
+						{error ? <div className='form-group__error'>{error}</div> : ""}
+						<br />
+						<button className='form-group__button' disabled={error}>
+							Calculate
+						</button>
+					</form>
+					<h2 className='form-group__result'>
+						Factorial: <span className='num-value'>{factorial}</span>
+					</h2>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
